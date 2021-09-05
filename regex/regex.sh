e5d4d3b930e37fe4bdea9907947612438e981676
Joshua Eklund <eklun124@rynite.morris.umn.edu>
2021-09-05 16:57:03 -0500

    Get script to work with r1
    This commit adds the necessary code to the regex.sh script so that it produces the correct output in the case of r1_input.txt

--- regex/regex.sh
+++ regex/regex.sh
@@ -1,4 +1,8 @@
 #!/usr/bin/env bash
 
 sed -E 's/\* ([a-zA-Z]+), ([a-zA-Z]+)/1. \1\n2. \2\n/' < r0_input.txt > r0_output.txt
+
+sed -En 's/\* I am ([a-zA-Z]+)\. My favorite sandwich is ([a-z]+)\./1. \1\n2. \2\n/p' < r1_input.txt > r1_output.txt
+
+
  
