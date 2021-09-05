a3b2cb711b5870b50d0bedd3d72d6387b31a4752
Joshua Eklund <eklun124@rynite.morris.umn.edu>
2021-09-05 16:31:38 -0500

    Remove extra ">" from regex.sh
    This commit removes the extra ">" from the ">>r0_output.txt" so that the script will overwrite the r0_output.txt file rather than appending to it.

--- regex/regex.sh
+++ regex/regex.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
-sed -E 's/\* ([a-zA-Z]+), ([a-zA-Z]+)/1. \1\n2. \2\n/' < r0_input.txt >> r0_output.txt
+sed -E 's/\* ([a-zA-Z]+), ([a-zA-Z]+)/1. \1\n2. \2\n/' < r0_input.txt > r0_output.txt
  
