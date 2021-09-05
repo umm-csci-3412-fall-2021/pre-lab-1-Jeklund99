b17c24dd6298d87b8f7d2b6e4b093b43728474dc
Joshua Eklund <eklun124@rynite.morris.umn.edu>
2021-09-05 18:03:50 -0500

    Simplify regex.sh
    This commit simplifies the regex expression for the sed command that deals with r2_input.txt.

--- regex/regex.sh
+++ regex/regex.sh
@@ -6,6 +6,6 @@ sed -E 's/\* ([a-zA-Z]+), ([a-zA-Z]+)/1. \1\n2. \2\n/' < r0_input.txt > r0_outpu
 #The -n flag in this line disables sed's automatic printing. The p right before the last quotation mark specifies that sed should only print out lines that match the regex expression. This ensures that the "I am awesome. I love puppies, but I don't like sandwiches." line doesn't appear in the r1_output.txt file.
 sed -En 's/\* I am ([a-zA-Z]+)\. My favorite sandwich is ([a-z]+)\./1. \1\n2. \2\n/p' < r1_input.txt > r1_output.txt
 
-sed -E 's/\* sandwich with ([a-zA-Z]+\.?[a-z]+\.?[a-z]+\.) ([a-z]+ [a-z]+)/1. \1\n2. \2\n/' < r2_input.txt > r2_output.txt
+sed -E 's/\* sandwich with ([a-zA-Z\.]+) ([a-z ]+)/1. \1\n2. \2\n/' < r2_input.txt > r2_output.txt
 
  
